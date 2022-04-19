fc9d3db44df2a51d07a7600ca856fe6975d9b907
Fellipe Duarte <fellipeduarte45@gmail.com>
2022-04-19 02:54:55 -0300

    Alteração da rota layout

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/msg', function(req,res){
     res.send("Essa msg é automática")
 })
 
+app.get('/layout', function(req,res){
+    res.send("Esse é o novo layout")
+})
+
+
 app.listen(3000, function(){
     console.log("conexão inicializada")
 })

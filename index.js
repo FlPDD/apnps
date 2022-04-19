07379a617e2a0ca256c5c4839a76c0f0ad988b88
Fellipe Duarte <fellipeduarte45@gmail.com>
2022-04-19 02:53:26 -0300

    Adicionando a rota sobre

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/msg', function(req,res){
     res.send("Essa msg é automática")
 })
 
+app.get('/sobre', function(req,res){
+    res.send("Esta pagina está sendo desenvolvida")
+})
+
 app.listen(3000, function(){
     console.log("conexão inicializada")
 })

be8a42f6a2407cfcc02b8a4cc2c69c40cf3bf81d
Fellipe Duarte <fellipeduarte45@gmail.com>
2022-04-19 03:10:49 -0300

    Configurações iniciais

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+const exp = require('constants')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine","ejs")
+
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', function(req,res){
     res.send("Olá fellipe")
 })
-
-app.get('/msg', function(req,res){
-    res.send("Essa msg é automática")
-})
-
-app.get('/sobre', function(req,res){
-    res.send("Esta pagina está sendo desenvolvida")
-})
-
-app.get('/layout', function(req,res){
-    res.send("Esse é o novo layout")
-})
-
 
 app.listen(3000, function(){
     console.log("conexão inicializada")
